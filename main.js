1854e9f6bb23808e9f323bf0a684f8337165ecf0
Jorge Valdez <valdez.a.jorge@gmail.com>
2018-02-27 16:48:46 -0600

    getting an error code, saving work to backtrack to issue

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const { PWCracker } = require('./lib/pwcracker');
 const main = () => {
   const pwc = new PWCracker('./corpus');
 
-  pwc.createTable().then(() => console.log(pwc.rainbow.size()));
+  return pwc.createTable()
+    .then(() => {
+      console.log(`Table size: ${pwc.rainbow.size()}\nTable collisions: ${pwc.rainbow.collisions}`);
+      return pwc.crackFile('../hashes.txt');
+    }).then((cracked) => {
+      console.log(JSON.stringify(cracked, null, 2));
+    });
 };
 
 main();
