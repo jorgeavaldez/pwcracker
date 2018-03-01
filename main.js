9b37602e5d7de3eabaa071e36a02d8e85a8c04e1
Jorge Valdez <valdez.a.jorge@gmail.com>
2018-02-28 21:45:03 -0600

    added prompt and help info

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
 const { PWCracker } = require('./lib/pwcracker');
 
-const main = () => {
-  const pwc = new PWCracker('./corpus');
+const chalk = require('chalk');
+const yargs = require('yargs');
+
+const main = ({ hashfile, corpus }) => {
+  console.log(chalk.magenta(`Creating table using files in ${chalk.yellow(corpus)}`));
+
+  const pwc = new PWCracker(corpus);
 
   const printRainbowStats = () => {
-    console.log(`SIZE: ${pwc.rainbow.size}\nCOLLISIONS: ${pwc.rainbow.collisions}`);
+    console.log(chalk.green(`Rainbow table created!`))
+    console.log(chalk.magenta(`SIZE: ${chalk.yellow('' + pwc.rainbow.size)}\nCOLLISIONS: ${chalk.yellow('' + pwc.rainbow.collisions)}`));
+
+    console.log(chalk.magenta(`\nParsing hashfile ${chalk.yellow(hashfile)}`));
   };
 
+  console.log(chalk.magenta('Creating rainbow table...'));
   return pwc.createTable()
     .then(printRainbowStats)
-    .then(() => pwc.crackFile('../hashes.txt'))
-    .then(cracked => console.log(Object.keys(cracked).length))
+    .then(() => pwc.crackFile(hashfile))
+    .then(cracked => console.log(chalk.green(`Number of passwords cracked: ${chalk.yellow('' + Object.keys(cracked).length)}`)))
     .catch((err) => console.log(err));
 };
 
-main();
+const args = yargs
+  .usage('Usage: $0 <hashfile> <outfile> [options]')
+  .command('<hashfile> <outfile>', 'parses given hashfile and dumps cracked passwords to outfile')
+  .example('$0 hashes.txt passwords.txt -c ./corpus/')
+  .alias('c', 'corpus')
+  .nargs('c', 1)
+  .describe('c', 'specify corpus directory')
+  .default('c', './corpus/')
+  .describe('hashfile', 'file of password hashes to crack')
+  .default('hashfile', '../hashes.txt')
+  .describe('outfile', 'output file of cracked passwords')
+  .default('outfile', 'passwords.txt')
+  .help('h')
+  .alias('h', 'help')
+  .epilog('Jorge Valdez javaldez@smu.edu CSE 3339')
+  .argv;
+
+main(args);
